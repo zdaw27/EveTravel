4e0febb85b2409df33cdcbc24ad3c0058b1824ff
zdaw27 <51247612+zdaw27@users.noreply.github.com>
2021-02-02 22:13:52 +0900

    인벤토리 UI 추가.
    인벤토리 UI 추가.

---
+++ Assets/Scripts/GameObjectActiver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectActiver : MonoBehaviour
+{
+    [SerializeField] private GameObject target = null;
+
+    public void OnActive()
+    {
+        target.SetActive(true);
+    }
+
+    public void OnDeActive()
+    {
+        target.SetActive(false);
+    }
+}
