3a2db1f39ee4f32d9330c4d4930da94555c3d450
zdaw27 <51247612+zdaw27@users.noreply.github.com>
2021-02-17 19:05:53 +0900

    맵 체인지 구현중.
    맵 체인지 구현중.

---
+++ Assets/Scripts/ScriptableObjects/MapTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EveTravel
+{
+    [CreateAssetMenu(fileName = "MapTable", menuName = "ScriptableObjects/MapTable", order = 1)]
+    public class MapTable : ScriptableObject
+    {
+        [SerializeField] private List<EveMap> maps = new List<EveMap>();
+
+        public List<EveMap> Maps { get => maps; set => maps = value; }
+    }
+}
