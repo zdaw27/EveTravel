9f0f6bd9109b12f2c368e2086cb3464e576bc553
zdaw27 <51247612+zdaw27@users.noreply.github.com>
2021-02-27 19:50:38 +0900

    EffectTest 클래스 추가.
    ReadMe 설명용으로 제작한 클래스.

---
+++ Assets/Scripts/EffectTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace EveTravel
+{
+    public class EffectTest : MonoBehaviour
+    {
+        [SerializeField]
+        private EffectRaiser effectRaiser;
+
+        private float time;
+        private int effectIndex = 0;
+        private int yOffset;
+        // Start is called before the first frame update
+
+        // Update is called once per frame
+        void Update()
+        {
+            time += Time.deltaTime;
+            if (time >= 0.2f)
+            {
+                
+                time = 0;
+                foreach (EffectManager.EffectType type in Enum.GetValues(typeof(EffectManager.EffectType)))
+                {
+                    effectRaiser.RaiseEffect(effectIndex * Vector3.right + yOffset * Vector3.down, type);
+                    effectIndex++;
+                }
+                effectIndex = 0;
+                yOffset++;
+
+            }
+        }
+    }
+}
