02ed52ac2f54baf34363026805b156824eb9cdd1
zdaw27 <51247612+zdaw27@users.noreply.github.com>
2021-03-03 22:19:00 +0900

    코드 컨벤션 정리.
    코드 컨벤션 정리.

--- Assets/Scripts/Editor/ScriptableObjects/EditorData.cs
+++ Assets/Scripts/Editor/ScriptableObjects/EditorData.cs
@@ -7,10 +7,14 @@ namespace EveTravel
     [CreateAssetMenu(fileName = "EditorData", menuName = "ScriptableObjects/Editor/EditorData", order = 1)]
     public class EditorData : ScriptableObject
     {
-        [SerializeField] private EveMap eveMap;
-        [SerializeField] private GameObject tile1;
-        [SerializeField] private GameObject tile2;
-        [SerializeField] private GameObject wallTile;
+        [SerializeField]
+        private EveMap eveMap;
+        [SerializeField]
+        private GameObject tile1;
+        [SerializeField]
+        private GameObject tile2;
+        [SerializeField]
+        private GameObject wallTile;
 
         public EveMap EveMap { get => eveMap; set => eveMap = value; }
         public GameObject Tile1 { get => tile1; set => tile1 = value; }
